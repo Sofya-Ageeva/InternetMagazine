225fa5cac6e840f677658b4ec23e0e4f14c78e0f
Агеева Софья <son4a1996@yandex.ru>
2026-04-15 19:24:23 +0300

    Реализация приватного режима доступа

--- src/category.py
+++ src/category.py
@@ -17,8 +17,27 @@ class Category:
 
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         # Автоматическое увеличение счётчиков
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: 'Product') -> None:
+        """Добавляет продукт в категорию и увеличивает счётчик."""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        """Геттер для приватного атрибута __products."""
+        result = []
+        for product in self.__products:
+            result.append(
+                f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+            )
+        return ''.join(result)
+
+    def get_products_list(self) -> List[Product]:
+        """Возвращает приватный список продуктов как обычный список для программной работы."""
+        return self.__products
