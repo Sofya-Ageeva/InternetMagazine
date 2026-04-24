54dad88f9551a418c8091f6dd6646631bab2f771
Агеева Софья <son4a1996@yandex.ru>
2026-04-24 16:15:41 +0300

    Вывод информации через __repr__

--- src/logging_mixin.py
+++ src/logging_mixin.py
@@ -1,8 +1,22 @@
 class LoggingMixin:
     """Миксин для логирования создания объектов. Печатает информацию о классе и параметрах при создании объекта."""
 
+    name: str
+    description: str
+    price: float
+    quantity: int
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        class_name = self.__class__.__name__
-        params = ', '.join([repr(arg) for arg in args] + [f"{k}={v!r}" for k, v in kwargs.items()])
-        print(f"{class_name}({params})")
+        print(repr(self))
+
+
+    def __repr__(self) -> str:
+        """Формирует строку вида ClassName с параметрами создания."""
+        args_list = [
+            repr(self.name),
+            repr(self.description),
+            repr(self.price),
+            repr(self.quantity)
+        ]
+        return f"{self.__class__.__name__}({', '.join(args_list)})"
